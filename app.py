8f9ad0a18d4c17dfe56b3881bdfa9ba2acbb4cb0
MagnusAustin <manastare27@gmail.com>
2024-12-26 21:42:44 +0530

    Added a basic landing page

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    skills = {
-        "workout": ["Push-ups", "Squats", "Planks", "Running"],
-        "it_skills": ["Python", "SQL", "Networking", "Cybersecurity"],
-        "miscellaneous": ["Cooking", "Time Management", "Public Speaking"]
-    }
-    return render_template('index.html', skills=skills)
+    return render_template('index.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
